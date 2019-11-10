0d51908f77d66cb0d0ea9e28fa4e9e4d78424a1a
Xevion <xevioni@yandex.com>
2019-11-09 20:44:44 -0600

    created formatting style

--- specs/app.py
+++ specs/app.py
@@ -1,4 +1,24 @@
-class SpecsWindows(object):
+class WindowsSpecs(object):
+    def __init__(self):
+        pass
+
+    @property
+    def mobo():
+        pass
+
+    @property
+    def cpu():
+        pass
+
+    @property
+    def gpu():
+        pass
+
+    @property
+    def os():
+        pass
+
+class LinuxSpecs(object):
     def __init__(self):
         pass
 
--- specs/cmd.py
+++ specs/cmd.py
@@ -5,6 +5,7 @@ import platform
 import shutil
 
 from . import log
+from .app import WindowsSpecs, LinuxSpecs
 
 log.info('')
 
@@ -21,11 +22,20 @@ def collect():
         if not shutil.which('msinfo32'):
             log.critical('msinfo32 not detected in path')
             raise click.ClickException('msinfo32 not detected in path - override to use linux OS specs detection via --linux')
-        print(
-            f'{key} : {value}' for key, value in {
-                'Motherboard' : 
-            }
+        model = WindowsSpecs()
+        output = """::: {} Specs :::
+                'Motherboard' : {motherboard},
+                'Processor' : {cpu},
+                'Graphics Card' : {gpu},
+                'Drives' : {disks}
+                """
+        output = output.format(
+            motherboard=model.mobo,
+            cpu=model.cpu,
+            gpu=model.gpu,
+            disks=model.disks
         )
+        print(output)
     else:
-        log.critical(f'Unspported Platform \"{systype}\/{sysname}" detected, failing')
+        log.critical(f'Unsupported Platform \"{systype}\/{sysname}" detected, failing')
         return
