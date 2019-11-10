ede4004fedd32a97b0203f118a8e113bfd2a2c39
Xevion <xevioni@yandex.com>
2019-11-09 20:32:15 -0600

    msinfo32 for processing on windows systems, app for creating a Windows Model for data collection and reporting

---
+++ specs/app.py
@@ -0,0 +1,19 @@
+class SpecsWindows(object):
+    def __init__(self):
+        pass
+
+    @property
+    def mobo():
+        pass
+
+    @property
+    def cpu():
+        pass
+
+    @property
+    def gpu():
+        pass
+
+    @property
+    def os():
+        pass
