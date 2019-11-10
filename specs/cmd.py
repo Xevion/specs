bc85ff2caf1a4f0dbdb64cb464c11ecc1e6b0cc5
Xevion <xevioni@yandex.com>
2019-11-09 20:30:52 -0600

    added in .gitignore for msinfo32 file, decided on Windows processing and data collection model

--- specs/cmd.py
+++ specs/cmd.py
@@ -1,5 +1,8 @@
 import click
-import cpuinfo
+import pprint
+import os
+import platform
+import shutil
 
 from . import log
 
@@ -10,6 +13,19 @@ def cli():
     pass
 
 @cli.command()
-@clic.option('-c', '--copy', default=False, show_default=True, help='Copy returned specs to the clipboard')
-def run():
-    log.info('specs.run')
+@click.option('-c', '--copy', default=False, show_default=True, help='Copy returned specs to the clipboard')
+def collect():
+    systype, sysname = os.name, platform.system()
+    if os.name == 'nt':
+        log.info(f'{systype}/{sysname} system detected')
+        if not shutil.which('msinfo32'):
+            log.critical('msinfo32 not detected in path')
+            raise click.ClickException('msinfo32 not detected in path - override to use linux OS specs detection via --linux')
+        print(
+            f'{key} : {value}' for key, value in {
+                'Motherboard' : 
+            }
+        )
+    else:
+        log.critical(f'Unspported Platform \"{systype}\/{sysname}" detected, failing')
+        return
